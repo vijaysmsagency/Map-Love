5f3c177ae3721638e82af66ca8508106a8092493
Vijay Rai <smswithagency@gmail.com>
2026-04-26 17:21:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from PIL import Image
 import io
 
 # API Keys (Railway Environment Variables से आएंगी)
-TELEGRAM_TOKEN = os.getenv("8095048825:AAEEoiluSycrAg-GTuzDMq2m7r3MhXihd9I")
-GEMINI_API_KEY = os.getenv("AIzaSyA9OHh36vyIpTrawumw3xv3DI1obt1WXy8")
+TELEGRAM_TOKEN = os.environ("8095048825:AAEEoiluSycrAg-GTuzDMq2m7r3MhXihd9I")
+GEMINI_API_KEY = os.environ("AIzaSyA9OHh36vyIpTrawumw3xv3DI1obt1WXy8")
 
 # Gemini Setup
 genai.configure(api_key=GEMINI_API_KEY)
